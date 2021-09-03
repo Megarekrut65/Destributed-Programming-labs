47775e2007e6b229d519e37dbdea4f3eec70b0f0
Megarekrut65 <bandalak28@gmail.com>
2021-09-03 11:12:38 +0300

    Added slider and two threads

--- Labs/src/com/company/part1/lab1/a/TwoThreadProgram.java
+++ Labs/src/com/company/part1/lab1/a/TwoThreadProgram.java
@@ -1,25 +1,26 @@
 package com.company.part1.lab1.a;
 
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
-class SliderEditor
-{
+class SliderEditor {
     JSlider slider;
 
     public SliderEditor(JSlider slider) {
         this.slider = slider;
     }
-    public synchronized void changeValue(int value)
-    {
-        int newValue = slider.getValue() + value;
-        if(newValue >= 10 && newValue <= 90)
-        {
+
+    public synchronized void changeValue(int value) {
+        slider.setValue(slider.getValue() + value);
+        /*int newValue = slider.getValue() + value;
+        if (newValue >= 10 && newValue <= 90) {
             slider.setValue(newValue);
-        }
+        }*/
     }
 }
-class MyRunnable implements Runnable
-{
+
+class MyRunnable implements Runnable {
     SliderEditor editor;
     int value;
 
@@ -30,28 +31,51 @@ class MyRunnable implements Runnable
 
     @Override
     public void run() {
-        while (!Thread.interrupted())
-        {
+        while (!Thread.interrupted()) {
             editor.changeValue(value);
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
-public class TwoThreadProgram {
-    static Thread thread1;
-    static Thread thread2;
-    static Thread createThread(int value, JSlider slider)
-    {
-        return new Thread(new MyRunnable(new SliderEditor(slider),value));
-    }
-    public static void main(String[] args) {
-        JFrame win = new JFrame("TwoThreadProgram");
-        win.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        win.setSize(800, 400);
-        win.setResizable(false);
-        win.setLocationRelativeTo(null);
-        JPanel panel = new JPanel();
 
+public class TwoThreadProgram {
+    static Thread threadRight;
+    static Thread threadLeft;
+
+    static Thread createThread(int value, JSlider slider) {
+        return new Thread(new MyRunnable(new SliderEditor(slider), value));
+    }
+
+    public static void main(String[] args) {
+        JFrame win = getFrame();
+        JPanel panel = new JPanel();
+        JSlider slider = getSlider();
+        threadRight = createThread(1, slider);
+        threadLeft = createThread(-1, slider);
+        threadRight.setPriority(Thread.MAX_PRIORITY);
+        threadLeft.setPriority(Thread.MIN_PRIORITY);
+        panel.add(slider);
         win.setContentPane(panel);
         win.setVisible(true);
+        threadRight.start();
+        threadLeft.start();
+    }
+
+    private static JSlider getSlider() {
+        JSlider slider = new JSlider(10,90,50);
+        return slider;
+    }
+
+    private static JFrame getFrame() {
+        JFrame win = new JFrame("TwoThreadProgram");
+        win.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        win.setSize(400, 300);
+        win.setResizable(false);
+        win.setLocationRelativeTo(null);
+        return win;
     }
 }
