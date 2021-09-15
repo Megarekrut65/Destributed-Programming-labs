2e6a2b80a7a024236509e6b4652470ad78d0cde0
Megarekrut65 <bandalak28@gmail.com>
2021-09-15 23:02:02 +0300

    Created demo parallel lab2 c

--- Labs/src/com/company/part1/lab2/c/WayProgram.go
+++ Labs/src/com/company/part1/lab2/c/WayProgram.go
@@ -1,20 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 type Monk struct {
 	power int
 }
 func findStronger(monks []Monk) Monk {
+	len:= len(monks)
+	if len == 1 {return monks[0]}
+	var wg sync.WaitGroup
+	wg.Add(2)
 	var monk1, monk2 Monk
-	fmt.Println("Len ", len(monks))
-	if len(monks) >= 2 {
-		monk1 = findStronger(monks[:len(monks)/2])
-		monk2 = findStronger(monks[len(monks)/2:])
-	}else if len(monks) == 1{
-		return monks[0]
-	}
-	if monk1.power > monk2.power {
+	go func() {
+		defer wg.Done()
+		monk1 = findStronger(monks[len/2:])
+	}()
+	go func() {
+		defer wg.Done()
+		monk2 = findStronger(monks[:len/2])
+	}()
+	wg.Wait()
+	if monk1.power > monk2.power{
 		return monk1
 	} else {return monk2}
 }
