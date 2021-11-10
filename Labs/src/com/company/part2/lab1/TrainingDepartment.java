4a5b2fda0daf96166ca206140af240cc200ffdc8
Megarekrut65 <bandalak28@gmail.com>
2021-11-10 18:20:25 +0200

    Added TrainingDepartment class

--- Labs/src/com/company/part2/lab1/TrainingDepartment.java
+++ Labs/src/com/company/part2/lab1/TrainingDepartment.java
@@ -1,4 +1,34 @@
 package com.company.part2.lab1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TrainingDepartment {
+    private List<Group> groups = new ArrayList<>();
+
+    public TrainingDepartment() {
+    }
+
+    public TrainingDepartment(List<Group> groups) {
+        this.groups = groups;
+    }
+
+    public List<Group> getGroups() {
+        return groups;
+    }
+
+    public void setGroups(List<Group> groups) {
+        this.groups = groups;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("TrainingDepartment{groups=");
+        for(var group:groups){
+            builder.append(group).append('\n');
+        }
+        builder.append('}');
+        return builder.toString();
+    }
 }
