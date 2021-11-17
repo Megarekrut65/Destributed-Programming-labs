f2a0afc99c6922a00aab8409a77d4c27d6e40cdc
Megarekrut65 <bandalak28@gmail.com>
2021-11-17 19:19:07 +0200

    Connected db

--- Labs/src/com/company/part2/lab2/Main.java
+++ Labs/src/com/company/part2/lab2/Main.java
@@ -1,7 +1,26 @@
 package com.company.part2.lab2;
-
+import java.lang.reflect.InvocationTargetException;
+import java.sql.*;
 public class Main {
     public static void main(String[] args) {
+        try {
+            String url = "jdbc:mysql://" + "localhost" + ":" + "3306" + "/" +
+                    "department";
+            var con = DriverManager.getConnection(url, "root", "BF4B9NvZZpZ_rt");
+            var stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT ID_ST, name FROM students");
+            System.out.println("Students:");
+            while (rs.next()) {
+                int id = rs.getInt("ID_ST");
+                String name = rs.getString("name");
+                System.out.println(">>" + id + "-" + name);
+            }
+            rs.close();
+            stmt.close();
+            con.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
 }
